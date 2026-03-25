6419b9d3c5a984acfe4a6d27f5ee829b43f17c4e
fullstack290623 <fullstack290623@gmail.com>
2026-03-25 19:56:40 +0200

    upload

---
+++ main.py
@@ -0,0 +1,13 @@
+
+'''
+1
+Write a function average_score(scores: list[int]) -> float:
+  that receives a list of exactly three integers (0–100)
+  and returns the average as a float
+
+Example: average_score([80, 90, 70]) → 80.0
+
+if got list not with 3 numbers then -> return None
+Or:
+  raise ValueError("Expected exactly three scores")
+'''
