2872a9dbbef6a9b84aff86895a2519598134749f
pythonai211225-rgb <pythonai211225@gmail.com>
2026-04-12 21:44:47 +0300

    Create sol.sql

---
+++ sol.sql
@@ -0,0 +1,12 @@
+SELECT * 
+FROM COMPANY
+WHERE SALARY > 20000 and age < 30;
+
+update COMPANY
+set ADDRESS='New York', SALARY = SALARY * 2
+where id = 22;
+
+-- update the age to 23 for Kim
+
+delete from COMPANY;
+where name like 'James';
